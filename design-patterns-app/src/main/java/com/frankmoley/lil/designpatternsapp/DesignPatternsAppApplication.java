952682deaef6ff7c9c3a0ce098e89a2cfa0ae473
datttrian <datttrian@gmail.com>
2023-10-03 18:41:06 +0000

    Factory pattern in action

--- design-patterns-app/src/main/java/com/frankmoley/lil/designpatternsapp/DesignPatternsAppApplication.java
+++ design-patterns-app/src/main/java/com/frankmoley/lil/designpatternsapp/DesignPatternsAppApplication.java
@@ -1,13 +1,13 @@
-package com.frankmoley.lil.designpatternapp;
+package com.frankmoley.lil.designpatternsapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DesignPatternAppApplication {
+public class DesignPatternsAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DesignPatternAppApplication.class, args);
+		SpringApplication.run(DesignPatternsAppApplication.class, args);
 	}
 
 }
--- design-patterns-app/src/test/java/com/frankmoley/lil/designpatternsapp/DesignPatternsAppApplicationTests.java
+++ design-patterns-app/src/test/java/com/frankmoley/lil/designpatternsapp/DesignPatternsAppApplicationTests.java
@@ -1,10 +1,10 @@
-package com.frankmoley.lil.designpatternapp;
+package com.frankmoley.lil.designpatternsapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DesignPatternAppApplicationTests {
+class DesignPatternsAppApplicationTests {
 
 	@Test
 	void contextLoads() {
