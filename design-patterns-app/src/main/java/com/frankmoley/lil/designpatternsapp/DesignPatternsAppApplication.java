28898ea645a15af0cb9f19acdac5f2c41c822d2c
datttrian <datttrian@gmail.com>
2023-10-03 19:48:26 +0000

    Prototype pattern in action

--- design-patterns-app/src/main/java/com/frankmoley/lil/designpatternsapp/DesignPatternsAppApplication.java
+++ design-patterns-app/src/main/java/com/frankmoley/lil/designpatternsapp/DesignPatternsAppApplication.java
@@ -2,12 +2,28 @@ package com.frankmoley.lil.designpatternsapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+
+import com.frankmoley.lil.designpatternsapp.prototype.ProtoFalse;
+import com.frankmoley.lil.designpatternsapp.prototype.ProtoTrue;
 
 @SpringBootApplication
 public class DesignPatternsAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DesignPatternsAppApplication.class, args);
+	}
+
+	@Bean
+	public ProtoFalse protoFalse() {
+		return new ProtoFalse();
+	}
+
+	@Bean
+	@Scope("prototype")
+	public ProtoTrue protoTrue() {
+		return new ProtoTrue();
 	}
 
 }
