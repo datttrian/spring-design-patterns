639f6b2e1ce6aecfe0039cfab663d50a1c14305f
datttrian <datttrian@gmail.com>
2023-10-03 18:44:57 +0000

    Factory pattern in action

---
+++ design-patterns-app/src/main/java/com/frankmoley/lil/designpatternsapp/factory/Pet.java
@@ -0,0 +1,13 @@
+package com.frankmoley.lil.designpatternsapp.factory;
+
+public interface Pet {
+    void setName(String name);
+
+    String getName();
+
+    String getType();
+
+    boolean isHungry();
+
+    void feed();
+}
