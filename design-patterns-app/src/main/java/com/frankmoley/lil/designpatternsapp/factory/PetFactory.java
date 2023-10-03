12c65253add9ea8ceb4127314d389c75af3c938e
datttrian <datttrian@gmail.com>
2023-10-03 18:51:37 +0000

    Update Factory pattern in action

--- design-patterns-app/src/main/java/com/frankmoley/lil/designpatternsapp/factory/PetFactory.java
+++ design-patterns-app/src/main/java/com/frankmoley/lil/designpatternsapp/factory/PetFactory.java
@@ -1,5 +1,18 @@
 package com.frankmoley.lil.designpatternsapp.factory;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class PetFactory {
     // TODO
+    public Pet createPet(String animalType) {
+        switch (animalType.toLowerCase()) {
+            case "dog":
+                return new Dog();
+            case "cat":
+                return new Cat();
+            default:
+                throw new UnsupportedOperationException("unknown animal type");
+        }
+    }
 }
