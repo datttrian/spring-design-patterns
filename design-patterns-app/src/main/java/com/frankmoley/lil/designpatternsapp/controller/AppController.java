8d34aafdeba3dfd3300f1d7f02048e2d6818be76
datttrian <datttrian@gmail.com>
2023-10-03 19:23:25 +0000

    Builder pattern in action

--- design-patterns-app/src/main/java/com/frankmoley/lil/designpatternsapp/controller/AppController.java
+++ design-patterns-app/src/main/java/com/frankmoley/lil/designpatternsapp/controller/AppController.java
@@ -1,18 +1,18 @@
 package com.frankmoley.lil.designpatternsapp.controller;
 
 import com.frankmoley.lil.designpatternsapp.builder.Contact;
+import com.frankmoley.lil.designpatternsapp.builder.ContactBuilder;
 import com.frankmoley.lil.designpatternsapp.factory.Pet;
 import com.frankmoley.lil.designpatternsapp.factory.PetFactory;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/")
@@ -34,8 +34,10 @@ public class AppController {
         return pet;
     }
 
+    @GetMapping("presidents")
     public List<Contact> getPresidents() {
         List<Contact> contacts = new ArrayList<>();
+
         Contact contact = new Contact();
         contact.setFirstName("George");
         contact.setLastName("Washington");
@@ -43,6 +45,9 @@ public class AppController {
 
         contacts.add(new Contact("John", "Adams", null));
 
+        contacts.add(new ContactBuilder().setFirstName("Thomas").setLastName("Jefferson").buildContact());
+
         return contacts;
     }
+
 }
