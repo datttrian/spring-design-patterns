e602440c4737c36e2be1e2b330ffd4189e92bbf9
datttrian <datttrian@gmail.com>
2023-10-03 18:55:41 +0000

    Update Factory pattern in action

--- design-patterns-app/src/main/java/com/frankmoley/lil/designpatternsapp/controller/AppController.java
+++ design-patterns-app/src/main/java/com/frankmoley/lil/designpatternsapp/controller/AppController.java
@@ -1,6 +1,8 @@
 package com.frankmoley.lil.designpatternsapp.controller;
 
 import com.frankmoley.lil.designpatternsapp.factory.Pet;
+import com.frankmoley.lil.designpatternsapp.factory.PetFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,9 +14,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class AppController {
 
+    @Autowired
+    private PetFactory petFactory;
+
     @GetMapping
     public String getDefault() {
         return "{\"message\": \"Hello World\"}";
     }
 
+    @PostMapping("adoptPet/{type}/{name}")
+    public Pet adoptPet(@PathVariable String type, @PathVariable String name) {
+        Pet pet = this.petFactory.createPet(type);
+        pet.setName(name);
+        pet.feed();
+        return pet;
+    }
 }
