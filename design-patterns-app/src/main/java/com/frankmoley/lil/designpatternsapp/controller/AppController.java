d19f062bc675f3055779f05ffc3f89b2d2236f3e
datttrian <datttrian@gmail.com>
2023-10-03 19:15:42 +0000

    Builder pattern in action

--- design-patterns-app/src/main/java/com/frankmoley/lil/designpatternsapp/controller/AppController.java
+++ design-patterns-app/src/main/java/com/frankmoley/lil/designpatternsapp/controller/AppController.java
@@ -1,7 +1,11 @@
 package com.frankmoley.lil.designpatternsapp.controller;
 
+import com.frankmoley.lil.designpatternsapp.builder.Contact;
 import com.frankmoley.lil.designpatternsapp.factory.Pet;
 import com.frankmoley.lil.designpatternsapp.factory.PetFactory;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,5 +32,17 @@ public class AppController {
         pet.setName(name);
         pet.feed();
         return pet;
+    }
+
+    public List<Contact> getPresidents() {
+        List<Contact> contacts = new ArrayList<>();
+        Contact contact = new Contact();
+        contact.setFirstName("George");
+        contact.setLastName("Washington");
+        contacts.add(contact);
+
+        contacts.add(new Contact("John", "Adams", null));
+
+        return contacts;
     }
 }
